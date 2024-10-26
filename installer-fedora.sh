fe5f75f5b1ead2f3f2cd050b83dc7248f1568228
yourmom64209 <2jes1366@gmail.com>
2024-10-26 14:37:14 -0700

    Create installer-fedora.sh

---
+++ installer-fedora.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/yourmom64209/mybspwmdots
+cd mybspwmdots
+mkdir ~/.config/bspwm
+cp bspwmrc ~/.config/bspwm/bspwmrc
+mkdir ~/.config/sxhkd 
+cp sxhkdrc ~/.config/sxhkd/sxhkdrc 
+mkdir ~/.config/polybar 
+cp config.ini ~/.config/polybar/config.ini
+reset 
+
+sudo dnf install bspwm sxhkd polybar dmenu -y
+reboot
