d82a82309e443f98d0b8267eb08957227c9562ed
yourmom64209 <2jes1366@gmail.com>
2024-10-26 14:39:03 -0700

    Create installer-debian.sh

---
+++ installer-debian.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/yourmom64209/mybspwmdots
+cd mybspwmdots
+mkdir ~/.config/bspwm
+cp bspwmrc ~/.config/bspwm/bspwmrc
+mkdir ~/.config/sxhkd 
+cp sxhkdrc ~/.config/sxhkd/sxhkdrc 
+mkdir ~/.config/polybar 
+cp config.ini ~/.config/polybar/config.ini
+reset 
+
+sudo apt install bspwm sxhkd polybar dmenu -y
+echo "complete"
+echo "complete"
+echo "complete"
+reboot
